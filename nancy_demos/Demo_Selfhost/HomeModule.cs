016f55960998105fabce0fceaa6b2732e35d3b11
Willian Santos <wilian.nsantos@msn.com>
2013-09-08 15:41:31 -0300

    exemplo selfhost

---
+++ nancy_demos/Demo_Selfhost/HomeModule.cs
@@ -0,0 +1,37 @@
+﻿using Nancy;
+using System;
+
+namespace Demo_Selfhost
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get["/"] = p => "Olá!!";
+
+            Get["/hi/{name}"] = p => 
+            {
+                return String.Format("Hello {0}", p.name);
+            };
+
+            Get["/form"] = p =>
+            {
+                return @"
+                <html>
+                   <head><meta charset='utf-8' /></head>
+                   <body>
+                      <form method='post' action='/form'>
+                        <input type='text' name='nome'>
+                        <input type='submit' value='requisição post'>
+                      </form>        
+                   </body>
+                </html>";
+            };
+
+            Post["/form"] = p =>
+            {
+                return String.Format("Pagina acessada via POST, nome postado {0}", Request.Form["nome"]);
+            };
+        }
+    }
+}
---
+++ nancy_demos/Demo_Selfhost/Program.cs
@@ -0,0 +1,20 @@
+﻿using Nancy.Hosting.Self;
+using System;
+
+namespace Demo_Selfhost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new NancyHost(new Uri("http://localhost:3000"));
+
+            host.Start();
+            Console.WriteLine("Server Iniciado...");
+            Console.WriteLine("Pressione uma tecla para encerrar");
+            Console.ReadKey();
+
+            host.Stop();
+        }
+    }
+}
