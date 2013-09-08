14ded0f10cfa3376ecb29a600ec87416749d3ebd
Willian Santos <wilian.nsantos@msn.com>
2013-09-08 16:19:01 -0300

    demo aspnethost

---
+++ nancy_demos/Demo_Aspnethost/HomeModule.cs
@@ -0,0 +1,14 @@
+﻿using Nancy;
+using System;
+using System.Web;
+
+namespace Demo_Aspnethost
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get["/"] = _ => "ASP.NET Hosting up and running!!!";
+        }
+    }
+}
