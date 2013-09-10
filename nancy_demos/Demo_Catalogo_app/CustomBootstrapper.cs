ecd0dffeb21706de9074a80004dc09b19cfd6f83
willianns <wilian.nsantos@msn.com>
2013-09-09 22:06:13 -0300

    static conventions

---
+++ nancy_demos/Demo_Catalogo_app/CustomBootstrapper.cs
@@ -0,0 +1,18 @@
+﻿using Nancy;
+using Nancy.Conventions;
+using System;
+
+namespace Demo_Catalogo_app
+{
+    public class CustomBootstrapper : DefaultNancyBootstrapper
+    {
+        protected override void ConfigureConventions(NancyConventions nancyConventions)
+        {
+            base.ConfigureConventions(nancyConventions);
+
+            nancyConventions.StaticContentsConventions.Add(
+                StaticContentConventionBuilder.AddDirectory("assets", @"public")
+            );
+        }
+    }
+}
